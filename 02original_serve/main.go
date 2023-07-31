8dcf07f575f501184117957cbd7289831931f625
taki-21 <taki.it.0208.21@gmail.com>
2023-08-01 00:31:11 +0900

    original mux

---
+++ 02original_serve/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type MyMux struct{}
+
+func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		handleIndex(w, r)
+	case "/greeting":
+		handleGreet(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "welcome golang server.")
+}
+func handleGreet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello guest!")
+}
+
+func main() {
+	port := "8080"
+	mux := &MyMux{}
+	log.Printf("Server listening on http://localhost:%s/", port)
+	log.Print(http.ListenAndServe(":"+port, mux))
+
+}
